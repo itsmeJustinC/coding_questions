09fb3232e377fac34196719061642493fa2e866a
Justin Ceiley <justincdeveloper@gmail.com>
2019-12-10 21:58:28 -0500

    initial commit

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, )
+    bio = models.TextField(max_length=500)
 
     def __str__(self):
         return f'{self.user.username} Profile'
